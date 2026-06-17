775e43714ecd6451f246e776e951d4d183b6836d
niaziy168 <niaziy168@gmail.com>
2026-06-17 13:19:08 -0400

    Add pausing and restart video

--- video_player/__init__.py
+++ video_player/__init__.py
@@ -6,22 +6,34 @@ import time
 badge.mode(HIRES)#change this to lores if your video is less than 160x120, to get better fps
 
 image_number = 1
-image_count = 25 #change this to how many images are in your video
+image_count = 45 #change this to how many images are in your video
+pause = 0
 
 image = image.load(f"/system/apps/video_player/images/ezgif-frame-{image_number:03d}.png")
 
 def update():
-    global image_number, image, image_count
+    global image_number, image, image_count, pause
     
 
     # Try to load the current image
     image = image.load(f"/system/apps/video_player/images/ezgif-frame-{image_number:03d}.png")
         
-    screen.blit(image, vec2(0, 0))
+    screen.blit(image, vec2(0, 0))# if the video is not centered on the screen change these numbers (default is 0,0)
         
     time.sleep(0) #change this to slow the fps, however the Tufty update speed is not very fast anyways
     
-    image_number = image_number+1
+
+    if badge.pressed(BUTTON_A):
+        pause = 0
+    
+    if pause == 0:
+        image_number = image_number+1
+    
+    if badge.pressed(BUTTON_B):
+        pause = 1
+    
+    if badge.pressed(BUTTON_C):
+        image_number = 1
     
     if image_number > image_count:
         image_number = 1
