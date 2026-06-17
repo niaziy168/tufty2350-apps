6c7e9149776fc1e352a9ec1a26659dc80f905cbf
niaziy168 <niaziy168@gmail.com>
2026-06-17 18:09:17 -0400

    UI upgrades and improved rewinding

--- video_player/__init__.py
+++ video_player/__init__.py
@@ -8,11 +8,13 @@ badge.mode(HIRES)#change this to lores if your video is less than 160x120, to ge
 image_number = 1
 image_count = 45 #change this to how many images are in your video
 pause = 0
+blinkeffectcounter = 0
 
+ignore_font = rom_font.ignore
 image = image.load(f"/system/apps/video_player/images/ezgif-frame-{image_number:03d}.png")
 
 def update():
-    global image_number, image, image_count, pause
+    global image_number, image, image_count, pause, blinkeffectcounter
     
 
     # Try to load the current image
@@ -28,12 +30,25 @@ def update():
     
     if pause == 0:
         image_number = image_number+1
+    else:
+        if blinkeffectcounter == 0:
+            screen.text('PAUSED', 10, 10)
+            blinkeffectcounter = 1
+        else:
+            blinkeffectcounter = 0
+
     
     if badge.pressed(BUTTON_B):
         pause = 1
+        screen.pen = color.white
+        screen.font = ignore_font
+        screen.text('PAUSED', 10, 10)
     
-    if badge.pressed(BUTTON_C):
-        image_number = 1
+    if badge.held(BUTTON_C):
+        image_number = image_number - 5
+        screen.text('REWIND', 220, 10)
+        if image_number < 1:
+            image_number = 1
     
     if image_number > image_count:
         image_number = 1
